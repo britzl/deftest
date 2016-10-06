439e165d64c16fa5528f3007e8ab2cf3315512dd
Björn Ritzl <bjorn.ritzl@king.com>
2016-10-06 10:42:33 +0200

    Added telescope syntax example and improved other example tests

--- test/test_http.lua
+++ test/test_http.lua
@@ -1,14 +1,14 @@
--- here we use the fact that all tests are run in a courotine to pause the
--- entire test until we get a response
-local function http_request(url, method, headers, post_data, options)
-	local co = coroutine.running()
-	http.request(url, method, function(self, id, response)
-		coroutine.resume(co, id, response)
-	end, headers or {}, post_data or nil, options or {})
-	return coroutine.yield()
-end
-
 return function()
+	-- here we use the fact that all tests are run in a courotine to pause the
+	-- entire test until we get a response
+	local function http_request(url, method, headers, post_data, options)
+		local co = coroutine.running()
+		http.request(url, method, function(self, id, response)
+			coroutine.resume(co, id, response)
+		end, headers or {}, post_data or nil, options or {})
+		return coroutine.yield()
+	end
+
 	describe("http", function()
 		test("http.request should return a response and status code", function()
 			local id, response = http_request("http://www.google.com", "GET")
--- test/test_vmath.lua
+++ test/test_vmath.lua
@@ -1,13 +1,5 @@
 return function()
 	describe("vmath", function()
-		before(function()
-			print("I'm run before each vmath test")
-		end)
-		
-		after(function()
-			print("I'm run after each vmath test")
-		end)
-		
 		test("Add two vectors", function()
 			local v1 = vmath.vector3(2, 3, 4)
 			local v2 = vmath.vector3(5, 6, 7)
