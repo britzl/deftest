56a4263aa87ea14fe62277b3917457cf0270365f
Björn Ritzl <bjorn.ritzl@king.com>
2016-10-06 11:37:09 +0200

    Updated regex to match sha1

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,7 +2,10 @@
 # http://d.defold.com/archive/5ca3dd134cc960c35ecefe12f6dc81a48f212d40/engine/linux/dmengine_headless
 # http://d.defold.com/archive/5ca3dd134cc960c35ecefe12f6dc81a48f212d40/bob/bob.jar
 
-SHA1=$(curl -s http://d.defold.com/stable/info.json | sed -n 's/\"sha1\": \"(.*)/\1/p')
+# {"version": "1.2.89", "sha1": "5ca3dd134cc960c35ecefe12f6dc81a48f212d40"}
+SHA1=$(curl -s http://d.defold.com/stable/info.json | sed 's/.*sha1": "\(.*\)".*/\1/')
+echo $SHA1
+
 curl -o dmengine_headless 'http://d.defold.com/archive/${SHA1}/engine/linux/dmengine_headless'
 curl -o bob.jar 'http://d.defold.com/archive/${SHA1}/bob/bob.jar'
 java -jar bob.jar --debug build
