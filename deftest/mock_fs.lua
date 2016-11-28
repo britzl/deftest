7a7a941be7c0bd478e69693d5deed6f206ae050a
Björn Ritzl <bjorn.ritzl@king.com>
2016-11-28 11:51:50 +0100

    Added mock of the file system

---
+++ deftest/mock_fs.lua
@@ -0,0 +1,321 @@
+local mock = require "deftest.mock"
+
+local M = {}
+
+local files = {}
+
+local fail_file_operations = false
+
+--- from http://lua-users.org/wiki/CopyTable
+local function deepcopy(orig)
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		copy = {}
+		for orig_key, orig_value in next, orig, nil do
+			copy[deepcopy(orig_key)] = deepcopy(orig_value)
+		end
+		setmetatable(copy, deepcopy(getmetatable(orig)))
+	else -- number, string, boolean, etc
+		copy = orig
+	end
+	return copy
+end
+
+function M.mock()
+	mock.mock(sys)
+	files = {}
+	sys.load.replace(function (file)
+		local t = files[file]
+		if not t then
+			t = {}
+		end
+		return t
+	end)
+	sys.save.replace(function (file, t)
+		files[file] = deepcopy(t)
+	end)
+
+	local tmpfile_count = 0
+	local output = nil
+	local input = nil
+	mock.mock(io)
+	mock.mock(os)
+	os.remove.replace(function(filename)
+		assert(not fail_file_operations, "os.remove error")
+		assert(filename)
+		if not files[filename] or fail_file_operations then
+			return nil, ("%s: No such file or directory"):format(filename)
+		end
+		files[filename] = nil
+		return true
+	end)
+	os.rename.replace(function(oldname, newname)
+		assert(not fail_file_operations, "os.rename error")
+		assert(oldname and newname)
+		if not files[oldname] or fail_file_operations then
+			return nil, "No such file or directory"
+		end
+		files[newname] = files[oldname]
+		files[oldname] = nil
+		return true
+	end)
+	os.tmpname.replace(function()
+		tmpfile_count = tmpfile_count + 1
+		local filename = "tmpfile" .. tostring(tmpfile_count) .. "_" .. tostring(socket.gettime())
+		files[filename] = ""
+		return filename
+	end)
+	io.open.replace(function(filename, mode)
+		assert(filename)
+		if fail_file_operations then
+			return nil, "Unable to open file"
+		end
+		
+		local closed = false
+		local mode = mode
+		local file_position = 1
+		local file = {
+			type = "file"
+		}
+
+		mode = mode or "r"
+		mode = mode:gsub("b", "")
+		
+		if mode == "w" then
+			files[filename] = ""
+		elseif mode == "w+" then
+			files[filename] = ""
+		elseif mode == "a" or mode == "a+" then
+			files[filename] = files[filename] or ""
+			file_position = 1 + #files[filename]
+		elseif mode == "r" or mode == "r+" then
+			if not files[filename] then
+				return nil, filename .. ": No such file or directory"
+			else
+				files[filename] = files[filename]
+			end
+		else
+			return nil, "Invalid mode"
+		end
+
+		file.write = function(self, ...)
+			assert(not closed, "attempt to use a closed file")
+			assert(not fail_file_operations, "io error")
+			if mode == "r" then
+				return nil, "Bad file descriptor"
+			end
+			local args = { ... }
+			for _,data in ipairs(args) do
+				if mode == "a+" then
+					file_position = 1 + #files[filename]
+				end
+				local s = tostring(data)
+				local before = files[filename]:sub(1, file_position - 1)
+				local after = files[filename]:sub(file_position + #s) or ""
+				
+				files[filename] = before .. s .. after
+				file_position = file_position + #s
+				
+				-- this is a bit weird but it matches actual behavior
+				if mode == "a+" then
+					file_position = file_position - 1
+				end
+			end
+			return true
+		end
+		file.close = function()
+			assert(not closed, "attempt to use a closed file")
+			assert(not fail_file_operations, "io error")
+			closed = true
+			file.type = "closed file"
+		end
+		file.read = function(self, ...)
+			assert(not closed, "attempt to use a closed file")
+			assert(not fail_file_operations, "io error")
+			if mode == "w" or mode == "a" then
+				return nil, "Bad file descriptor"
+			end
+			
+			local function is_eof()
+				return file_position == #files[filename] + 1
+			end
+			
+			local formats = { ... }
+			if #formats == 0 then
+				formats[1] = "*l"
+			end
+			local results = {}
+			for i=1,#formats do
+				local format = formats[i]
+				if format == "*n" then
+					if is_eof() then
+						results[i] = nil
+					else
+						local s = files[filename]:sub(file_position)
+						local n = s:match("([-+]?%d+%.?%d*).*")
+						if n then
+							results[i] = tonumber(n)
+							file_position = file_position + #n
+						else
+							results[i] = nil
+						end
+					end
+				elseif format == "*a" then
+					if is_eof() then
+						results[i] = ""
+					else
+						local s = files[filename]:sub(file_position)
+						results[i] = s
+						file_position = #files[filename] + 1
+					end
+				elseif format == "*l" then
+					local s = files[filename]:sub(file_position)
+					local line = s:match("[^\r\n]+")
+					if line then
+						file_position = file_position + #line + 1 -- +1 = the linebreak
+					end
+					results[i] = line
+				elseif type(format) == "number" then
+					local length = format
+					if length < 0 then
+						results[i] = nil
+					elseif is_eof() then
+						results[i] = nil
+					elseif length == 0 then
+						results[i] = ""
+					else
+						local s = files[filename]:sub(file_position, file_position + length - 1)
+						file_position = file_position + #s
+						results[i] = s
+					end
+				else
+					results[i] = nil
+				end
+			end
+			return unpack(results)
+		end
+		file.flush = function()
+			assert(not closed, "attempt to use a closed file")
+			assert(not fail_file_operations, "io error")
+		end
+		file.lines = function()
+			assert(not closed, "attempt to use a closed file")
+			assert(not fail_file_operations, "io error")
+			return files[filename]:gmatch("[^\r\n]+")
+		end
+		file.seek = function(self, whence, offset)
+			whence = whence or "cur"
+			offset = offset or 0
+			if whence == "set" then
+				if offset < 0 then
+					return nil, "Invalid argument"
+				end
+				file_position = 1 + offset
+				return offset
+			elseif whence == "cur" then
+				if file_position + offset < 1 then
+					return nil, "Invalid argument"
+				end
+				file_position = file_position + offset
+				return file_position
+			elseif whence == "end" then
+				file_position = 1 + #files[filename] + offset
+				return file_position
+			end
+		end
+		return file
+	end)
+	io.type.replace(function(obj)
+		return obj and obj.type or nil
+	end)
+	io.write.replace(function(...)
+		if not output then
+			return
+		end
+		output.write(...)
+	end)
+	io.read.replace(function(...)
+		if not input then
+			return
+		end
+		return input.read(...)
+	end)
+	io.read.replace(function(...)
+		if not input then return end
+		return input.read(...)
+	end)
+	io.output.replace(function(file)
+		if not file then
+			return output
+		end
+		if type(file) == "string" then
+			output = io.open(file)
+		else
+			output = file
+		end
+		return output
+	end)
+	io.input.replace(function(file)
+		if not file then
+			return input
+		end
+		if type(file) == "string" then
+			input = io.open(file)
+		else
+			input = file
+		end
+		return input
+	end)
+	io.flush.replace(function()
+		if output then
+			output.flush()
+		end
+	end)
+	io.close.replace(function(file)
+		if file then
+			file.close()
+		elseif output then
+			io.output().close()
+		end
+	end)
+	io.lines.replace(function(filename)
+		if filename then
+			local file = io.open(filename)
+			return file.lines()
+		elseif input then
+			return io.input().lines()
+		end
+	end)
+	io.popen.replace(function(prog, mode)
+		error("io.popen is not mockable")
+	end)
+	io.tmpfile.replace(function()
+		return io.open(os.tmpname())
+	end)
+end
+
+function M.unmock()
+	mock.unmock(sys)
+	mock.unmock(io)
+	mock.unmock(os)
+end
+
+
+function M.has_file(file)
+	return files[file] ~= nil
+end
+
+function M.set_file(file, content)
+	files[file] = content
+end
+
+function M.fail()
+	fail_file_operations = true
+end
+
+function M.success()
+	fail_file_operations = false
+end
+
+return M
---
+++ test/test_mock_fs.lua
@@ -0,0 +1,278 @@
+return function()
+	local mock_fs = require "deftest.mock_fs"
+	
+	describe("mock_fs", function()
+		before(function()
+			mock_fs.mock()
+		end)
+		
+		after(function()
+			mock_fs.unmock()
+		end)
+
+		it("should not write to actual files on disk", function()
+			mock_fs.mock()
+			local function read(filename)
+				local f = io.open(filename, "rb")
+				if not f then
+					return nil
+				end
+				local d = f:read("*a")
+				f:close()
+				return d
+			end
+			
+			local function is_empty(filename)
+				local d = read(filename)
+				return not d or d == ""
+			end
+						
+			mock_fs.unmock()
+
+			local filename1 = os.tmpname()
+			local filename2 = os.tmpname()
+			os.remove(filename1)
+			os.remove(filename2)
+			
+			assert(is_empty(filename1), "Expected first file to be empty")
+			assert(is_empty(filename2), "Expected second file to be empty")
+			
+			mock_fs.mock()
+			
+			sys.save(filename1, { foo = "bar" })
+			local f = io.open(filename2, "w")
+			f:write("foobar")
+			f:close()
+			
+			mock_fs.unmock()
+			
+			assert(is_empty(filename1), "Expected first file to be empty after writing to it while fs is mocked")
+			assert(is_empty(filename2), "Expected second file to be empty after writing to it while fs is mocked")
+		end)
+		
+		it("should mock sys.*", function()
+			--mock_fs.unmock()
+			sys.save("filename1", { foo = "bar" })
+			local t = sys.load("filename1")
+			assert(t.foo == "bar")
+		end)
+		
+		context("Mocked io.* functions", function()
+		
+			it("should not create any files until they are written to", function()
+				--mock_fs.unmock()
+				local f = io.open("ihopethisfiledoesnotexist" .. tostring(os.time()), "r")
+				assert(not f)
+			end)
+			
+			it("should allow the creation of a temporary file and it should be created automatically", function()
+				--mock_fs.unmock()
+				local f = io.open(os.tmpname(), "r")
+				assert(f)
+			end)
+			
+			it("should have a function to check if a file is open or closed", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				assert(io.type(f) == "file")
+				f:close()
+				assert(io.type(f) == "closed file")
+			end)
+			
+			it("should be able to read from and write to files", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				f:write("foobar")
+				f:close()
+				
+				local f = io.open(filename, "r")
+				local d = f:read("*a")
+				f:close()
+				assert(d == "foobar")
+			end)
+			
+			it("should be able to append to files", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				f:write("foobar")
+				f:close()
+				
+				local f = io.open(filename, "a")
+				f:write("foobar")
+				f:close()
+	
+				local f = io.open(filename, "r")
+				local d = f:read("*a")
+				f:close()
+				assert(d == "foobarfoobar")
+			end)
+			
+			it("should only write to the end of the file in append update mode (a+) ", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				f:write("foobar")
+				f:close()
+				
+				local f = io.open(filename, "a+")
+				f:seek("set", 3)	-- this will have no effect on the write below
+				f:write("foobar")
+				assert(f:seek("cur") == 12)
+				f:close()
+	
+				local f = io.open(filename, "r")
+				local d = f:read("*a")
+				f:close()
+				assert(d == "foobarfoobar", d)
+			end)
+			
+			it("should be able to read line by line from a file", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				f:write("line1\nline2\nline3\n")
+				f:close()
+				
+				local f = io.open(filename, "r")
+				assert(f:read("*l") == "line1")
+				assert(f:read("*l") == "line2")
+				assert(f:read("*l") == "line3")
+				assert(f:read("*l") == nil)
+			end)
+			
+			it("should be able to read a certain number of characters from a file", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				f:write("abcde")
+				f:close()	
+				
+				local f = io.open(filename, "r")
+				assert(f:read(2) == "ab")
+				assert(f:read(2) == "cd")
+				assert(f:read(2) == "e")
+				assert(f:read(2) == nil)
+			end)
+			
+			it("should be able to read a number from a file", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				f:write("12a34.56b-78c-90.12d")
+				f:close()
+				
+				local f = io.open(filename, "r")
+				assert(f:read("*n") == 12)
+				assert(f:read(1) == "a")
+				assert(f:read("*n") == 34.56)
+				assert(f:read(1) == "b")
+				assert(f:read("*n") == -78)
+				assert(f:read(1) == "c")
+				assert(f:read("*n") == -90.12)
+				assert(f:read(1) == "d")
+				assert(f:read("*n") == nil)
+			end)
+			
+			it("should be able to read lines from a file", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				f:write("line1\nline2\nline3\n")
+				f:close()
+				
+				local f = io.open(filename, "r")
+				local lines = {}
+				for line in f:lines() do
+					lines[#lines + 1] = line
+				end
+				assert(#lines == 3)
+				assert(lines[1] == "line1")
+				assert(lines[2] == "line2")
+				assert(lines[3] == "line3")
+			end)
+
+			it("should be able to read multiple values in one call to io.read()", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				f:write("abcline1\n123line3\nline4\n")
+				f:close()
+	
+				local f = io.open(filename, "r")
+				local r1, r2, r3, r4 = f:read(3, "*l", "*n", "*a")
+				assert(r1 == "abc")
+				assert(r2 == "line1")
+				assert(r3 == 123)
+				assert(r4 == "line3\nline4\n")
+			end)
+
+			it("should be able to write multiple values to a file", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				assert(f:write("ab", "cd", "ef"))
+				f:close()
+				
+				local f = io.open(filename, "r")
+				assert(f:read("*a") == "abcdef")
+			end)
+
+			it("should not be able to write in read mode", function()
+				local f = io.open(os.tmpname(), "r")
+				local ok, err = f:write("abc")
+				assert(not ok and err)
+			end)
+
+			it("should not be able to read in write mode", function()
+				local f = io.open(os.tmpname(), "w")
+				local s, err = f:read()
+				assert(not s and err)
+			end)
+
+			it("should not be able to read in append mode", function()
+				local f = io.open(os.tmpname(), "a")
+				local s, err = f:read()
+				assert(not s and err)
+			end)
+			
+			it("should be able to seek and read in a file", function()
+				--mock_fs.unmock()
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				assert(f:write("abcdefg"))
+				f:close()
+				
+				local f = io.open(filename, "r")
+				assert(f:read("*a") == "abcdefg")
+				assert(f:seek("set") == 0)
+				assert(f:read("*a") == "abcdefg")
+				assert(f:seek("set", 1) == 1)
+				assert(f:read("*a") == "bcdefg")
+	
+				f:seek("set")
+				f:seek("cur", 1)
+				assert(f:read("*a") == "bcdefg")
+				f:seek("cur", -1)
+				assert(f:read("*a") == "g")
+				f:seek("end", -1)
+				assert(f:read("*a") == "g")
+			end)
+			
+			it("should be able to seek and write in a file", function()
+				--mock_fs.unmock()			
+				local filename = os.tmpname()
+				local f = io.open(filename, "w")
+				assert(f:write("abcdefg"))
+				f:seek("set", 1)
+				f:write("x")
+				f:close()
+				
+				local f = io.open(filename, "r")
+				assert(f:read("*a") == "axcdefg")
+			end)
+		end)
+	end)
+end
