1a01c23ae6bd37965ea557c58b4b14b8e06c3759
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-07-30 15:14:25 +0200

    io.popen not working properly

--- deftest/luacov/runner.lua
+++ deftest/luacov/runner.lua
@@ -303,17 +303,7 @@ local function is_absolute(path)
 end
 
 local function get_cur_dir()
-   local pwd_cmd = dir_sep == "\\" and "cd 2>nul" or "pwd 2>/dev/null"
-   local handler = io.popen(pwd_cmd, "r")
-   local cur_dir = handler:read()
-   handler:close()
-   cur_dir = cur_dir:gsub("\r?\n$", "")
-
-   if cur_dir:sub(-1) ~= dir_sep and cur_dir:sub(-1) ~= "/" then
-      cur_dir = cur_dir .. dir_sep
-   end
-
-   return cur_dir
+   return ""
 end
 
 -- Sets configuration. If some options are missing, default values are used instead.
