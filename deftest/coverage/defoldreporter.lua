e5f54da7bb727ffcdd0959cf1a6be043352b5bc5
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-07-31 15:07:11 +0200

    Fixed filenames

--- deftest/coverage/defoldreporter.lua
+++ deftest/coverage/defoldreporter.lua
@@ -1,5 +1,42 @@
 local reporter = require "luacov.reporter"
 
+local function exists(filename)
+	local file = io.open(filename, "r")
+	if not file then
+		return false
+	end
+	io.close(file)
+	return true
+end
+
+local function fix_filename(filename)
+	local filename = filename:gsub("^=", "")
+	if exists(filename) then
+		return filename
+	end
+	-- foo.bar -> foo/bar.lua
+	local parsed_name = filename:gsub("%.", "/")..".lua"
+	if exists(parsed_name) then
+		return parsed_name
+	end
+	-- foobar.scriptc -> foobar.script
+	parsed_name = filename:gsub("^/", ""):gsub("%.scriptc$$", ".script")
+	if exists(parsed_name) then
+		return parsed_name
+	end
+	-- foobar.gui_scriptc -> foobar.gui_script
+	parsed_name = filename:gsub("^/", ""):gsub("%.gui_scriptc$$", ".gui_script")
+	if exists(parsed_name) then
+		return parsed_name
+	end
+	-- foobar.gui_scriptc -> foobar.gui_script
+	parsed_name = filename:gsub("^/", ""):gsub("%.render_scriptc$$", ".render_script")
+	if exists(parsed_name) then
+		return parsed_name
+	end
+	return filename
+end
+
 local DefoldReporter = setmetatable({}, reporter.DefaultReporter) do
 	DefoldReporter.__index = DefoldReporter
 
@@ -12,17 +49,15 @@ local DefoldReporter = setmetatable({}, reporter.DefaultReporter) do
 		-- from files on disk when run in Defold
 		local d = {}
 		for filename,stats in pairs(self._data) do
-			d[filename:gsub("=/", ""):gsub("=", "")] = stats
+			d[fix_filename(filename)] = stats
 		end
 		self._data = d
 		for i,filename in pairs(self._files) do
-			self._files[i] = filename:gsub("=/", ""):gsub("=", "")
+			self._files[i] = fix_filename(filename)
 		end
 		
 		for _, filename in ipairs(self:files()) do
-			-- check if file exists
-			local file = io.open(filename, "r")
-			if file then
+			if exists(filename) then
 				print("  Processing:", filename)
 				self:_run_file(filename)
 			else
