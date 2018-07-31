392c6c99efdf95c534edef03621997008ae457c7
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-07-31 14:45:23 +0200

    Changed how current dir is retrieved

--- luacov/runner.lua
+++ luacov/runner.lua
@@ -296,11 +296,15 @@ local function is_absolute(path)
 end
 
 local function get_cur_dir()
-   local pwd_cmd = dir_sep == "\\" and "cd 2>nul" or "pwd 2>/dev/null"
-   local handler = io.popen(pwd_cmd, "r")
-   local cur_dir = handler:read()
-   handler:close()
-   cur_dir = cur_dir:gsub("\r?\n$", "")
+   local curdir
+   pcall(function()
+      local pwd_cmd = dir_sep == "\\" and "cd 2>nul" or "pwd 2>/dev/null"
+      local handler = io.popen(pwd_cmd, "r")
+      cur_dir = handler:read()
+      handler:close()
+      cur_dir = cur_dir:gsub("\r?\n$", "")
+   end)
+   cur_dir = cur_dir or os.getenv("PWD") or "."
 
    if cur_dir:sub(-1) ~= dir_sep and cur_dir:sub(-1) ~= "/" then
       cur_dir = cur_dir .. dir_sep
