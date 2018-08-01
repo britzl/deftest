4bdbbd43d44059bfd4c0dc4eb29916969746fa11
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-08-01 07:08:34 +0200

    reverted reporter file check

--- luacov/reporter/defold.lua
+++ luacov/reporter/defold.lua
@@ -15,17 +15,7 @@ local function fix_filename(filename)
 		return filename
 	end
 
-	local path, extension = filename:match("(.*)%.(%a*)$")
-	if path and extension then
-		path = path:gsub("%.", "/")
-		local parsed_name = path .. "." .. extension
-		print(filename, parsed_name)
-		if exists(parsed_name) then
-			return parsed_name
-		end
-	end
-	return filename
---[[	-- foo.bar -> foo/bar.lua
+	-- foo.bar -> foo/bar.lua
 	local parsed_name = filename:gsub("%.", "/")..".lua"
 	if exists(parsed_name) then
 		return parsed_name
@@ -45,7 +35,7 @@ local function fix_filename(filename)
 	if exists(parsed_name) then
 		return parsed_name
 	end
-	return filename--]]
+	return filename
 end
 
 local DefoldReporter = setmetatable({}, reporter.DefaultReporter) do
