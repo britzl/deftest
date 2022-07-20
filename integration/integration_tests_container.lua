f1eea1259351b715c18f85631b761f52e863db84
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2022-07-21 00:49:10 +0300

    Temporary functions for creating integration tests are added

--- integration/integration_tests_container.lua
+++ integration/integration_tests_container.lua
@@ -15,4 +15,28 @@ function C.add_processed_test(test_name, test)
 	C.processed_tests[test_name] = test
 end
 
+-- Function that creates integration test that triggers when messege is received.
+-- TODO - change for telescope-like syntax.
+function message_test(name, before, message_id, sender, after, max_time)
+	local test = {}
+	test.type = "message"
+	test.before = before
+	test.message_id = message_id
+	test.sender = sender
+	test.after = after
+	test.max_time = max_time
+	return test
+end
+
+-- Function that creates integration test that triggers after a given time.
+-- TODO - change for telescope-like syntax.
+function wait_test(name, before, after, max_time)
+	local test = {}
+	test.type = "wait"
+	test.before = before
+	test.after = after
+	test.max_time = max_time
+	return test
+end
+
 return C
