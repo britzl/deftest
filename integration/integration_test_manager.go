647f39db53a2d0ee1f9066b2aff6bca6640444a6
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2022-07-20 14:02:54 +0300

    Integration tests architecture is updated

---
+++ integration/integration_test_manager.go
@@ -0,0 +1,33 @@
+components {
+  id: "integration_test_manager"
+  component: "/integration/integration_test_manager.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/integration/testing_object.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
