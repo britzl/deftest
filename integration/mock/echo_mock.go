9c0e5f8d8edca155619b2d0baa9cf51976b19197
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2022-07-21 19:42:59 +0300

    Message receiving tests work

---
+++ integration/mock/echo_mock.go
@@ -0,0 +1,15 @@
+components {
+  id: "echo_mock"
+  component: "/integration/mock/echo_mock.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- test/test_integration_tests.lua
+++ test/test_integration_tests.lua
@@ -1,7 +1,15 @@
 return function()
-	wait_test("should execute tests with the type 'wait' after a time period", function() end, function()
+	wait_test("should execute wait_tests after a time period", function() end, function()
 		return function()
 			assert(true)
 		end 
 	end, 1)
+
+	message_test("should execute message_tests when a message is received", function()
+		msg.post("/echo_mock#echo_mock", "echo test", {text = "test"})
+	end, "echo", "/echo_mock#echo_mock", function(message)
+		return function()
+			assert(message.text == "test")
+		end 
+	end, 10)
 end
